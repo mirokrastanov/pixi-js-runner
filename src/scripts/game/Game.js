ed7083401a42bc1dbac4e913ff5567ae0745b002
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-15 22:28:09 +0300

    Add a way to restart the game and a play button

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -54,6 +54,31 @@ export class Game extends Scene {
         this.container.on("pointerdown", () => {
             this.hero.startJump();
         });
+        this.hero.sprite.once("die", () => {
+            document.querySelector('#overlay').style.display = "block";
+            document.querySelector('#play-again').style.display = "block";
+            document.querySelector('#final-score').style.display = "block";
+            document.querySelector('#final-score').textContent = `Score: ${this.hero.score}`;
+
+            document.querySelector('#play-again').addEventListener('click', this.playAgain);
+        });
+    }
+
+    playAgain(e) {
+        console.log(e);
+        document.querySelector('#overlay').style.display = "none";
+        document.querySelector('#play-again').style.display = "none";
+        document.querySelector('#final-score').style.display = "none";
+        App.scenes.start("Game");
+    }
+
+    destroy() {
+        Matter.Events.off(App.physics, 'collisionStart', this.onCollisionStart.bind(this));
+        App.app.ticker.remove(this.update, this);
+        this.bg.destroy();
+        this.hero.destroy();
+        this.platforms.destroy();
+        this.labelScore.destroy();
     }
 
     createBackground() {
