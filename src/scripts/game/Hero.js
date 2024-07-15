d5aa024f25fe81a5855f4975aa3a0180771cb6b7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-15 19:34:01 +0300

    Add physical body to the hero

--- src/scripts/game/Hero.js
+++ src/scripts/game/Hero.js
@@ -1,9 +1,24 @@
 import * as PIXI from "pixi.js";
+import * as Matter from 'matter-js';
 import { App } from '../system/App.js';
 
 export class Hero {
     constructor() {
         this.createSprite();
+        this.createBody();
+        App.app.ticker.add(this.update.bind(this));
+    }
+
+    // [07]
+    createBody() {
+        this.body = Matter.Bodies.rectangle(this.sprite.x + this.sprite.width / 2, this.sprite.y + this.sprite.height / 2, this.sprite.width, this.sprite.height, { friction: 0 });
+        Matter.World.add(App.physics.world, this.body);
+        this.body.gameHero = this;
+    }
+
+    update() {
+        this.sprite.x = this.body.position.x - this.sprite.width / 2;
+        this.sprite.y = this.body.position.y - this.sprite.height / 2;
     }
 
     createSprite() {
