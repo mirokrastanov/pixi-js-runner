bd7dd2e714699cdd2543acd735490b9d1a608053
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-15 18:47:51 +0300

    Create a moving background

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -2,8 +2,9 @@ import { Game } from "./Game";
 import { Tools } from "../system/Tools";
 
 export const Config = {
+    bgSpeed: 2,
     loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
     scenes: {
         "Game": Game
-    }
+    },
 };
